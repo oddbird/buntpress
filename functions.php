a39c90112c221e9b3a28da64b2df1de2d33cceba
Miriam Suzanne <miriam@oddbird.net>
2016-08-19 13:03:45 -0600

    Auto-complete?

--- functions.php
+++ functions.php
@@ -179,3 +179,16 @@ function buntpress_override_checkout_fields( $fields ) {
 }
 add_filter( 'woocommerce_checkout_fields' , 'buntpress_override_checkout_fields' );
 
+
+/**
+ * Auto Complete all WooCommerce orders.
+ */
+add_action( 'woocommerce_thankyou', 'custom_woocommerce_auto_complete_order' );
+function custom_woocommerce_auto_complete_order( $order_id ) {
+    if ( ! $order_id ) {
+        return;
+    }
+
+    $order = wc_get_order( $order_id );
+    $order->update_status( 'completed' );
+}
