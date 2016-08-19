240844e671caba1c83e6c2516ab646a1897fb31c
Miriam Suzanne <miriam@oddbird.net>
2016-08-19 17:21:32 -0600

    Custom email message

---
+++ woocommerce/templates/emails/customer-completed-order.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Customer completed order email
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/emails/customer-completed-order.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates/Emails
+ * @version     2.5.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * @hooked WC_Emails::email_header() Output the email header
+ */
+do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
+
+<p>
+  Thanks for making a reservation at
+  <a href="http://buntport.com"><? echo get_option( 'blogname' ) ?></a href="buntport.com">!
+  The box office will open a half hour before show time.
+  Seating is general admission.
+  We will hold your reservation until 5 minutes before curtain,
+  then make them available to standby customers.
+  Call 720-946-1388 if you have any questions.
+  See below for details on your order.
+</p>
+
+<?php
+
+/**
+ * @hooked WC_Emails::order_details() Shows the order details table.
+ * @hooked WC_Emails::order_schema_markup() Adds Schema.org markup.
+ * @since 2.5.0
+ */
+do_action( 'woocommerce_email_order_details', $order, $sent_to_admin, $plain_text, $email );
